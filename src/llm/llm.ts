dfa67941dd4e348c66409cdcef3b30d1ea8fc15c
Rahel Lüthy <rahel.luethy@fhnw.ch>
2024-11-16 12:15:14 +0100

    #1: Bump lib versions

--- src/llm/llm.ts
+++ src/llm/llm.ts
@@ -1,6 +1,8 @@
 import { ChatOpenAI } from '@langchain/openai'
 
 export const createLlm = () =>
+  // Optionally switch to ChatOllama instead of OpenAI
+  // However, Ollama currently does not support tool choice yet (https://github.com/langchain-ai/langchainjs/issues/6897#issuecomment-2384210852)
   new ChatOpenAI({
     model: import.meta.env.VITE_LLM_MODEL,
     apiKey: import.meta.env.VITE_LLM_API_KEY,
